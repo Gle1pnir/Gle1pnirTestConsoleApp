49fef9393f98050039eb82f4de64028ecde15707
Gle1pnir <119879265+Gle1pnir@users.noreply.github.com>
2022-12-05 20:39:00 +0300

    -Vlad

--- Gle1pnirTestConsoleApp/Program.cs
+++ Gle1pnirTestConsoleApp/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Vlad");
+Console.WriteLine("Hello, Boris");
+Console.WriteLine("Bye, Vlad!");
 Console.ReadLine();
